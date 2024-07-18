a995007af8577511a5fd838d4137ab78d6204f80
JohnLouiene <johnlouiene@gmail.com>
2024-07-18 09:57:40 +0800

    Add files via upload

---
+++ include/nav.php
@@ -0,0 +1,20 @@
+<ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="forms.php">Forms</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="get_all_product.php">Product</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="user_signin.php">User Account</a>
+                </li>
+                <li>
+                    <form action="searchresult_with_table.php" method="POST">
+                        <input type="text" name="search" id="search">
+                        <input type="submit" value="Search">
+                    </form>
+                </li>
+            </ul>
